511ef2b0cb09c2879d2e13b0d4131a77ceba1a06
haidul12 <haidulmahfud12@gmail.com>
2024-12-07 18:53:09 +0800

    Add files via upload

---
+++ PROG 8.py
@@ -0,0 +1,32 @@
+
+
+a = 24 
+b = 30
+
+#operasi kali X
+hasil = a * b 
+print("nilai dari a * b =",hasil)
+
+#operasi bagi :
+hasil = a / b 
+print("nilai dari a / b =",hasil)
+
+#operasi tambah +
+hasil = a + b 
+print("nilai dari a + b =",hasil)
+
+#operasi kurang -
+hasil = a - b 
+print("nilai dari a - b=",hasil)
+
+#operasi perpangkatan **
+hasil = a ** b 
+print("nilai dari a ** b=",hasil)
+
+#modulus (sisa bagi) %
+hasil = a % b 
+print("nilai dari a mod b=",hasil)
+
+#floor dividion //
+hasil = a // b 
+print("nilai dari a flor b=",hasil)
